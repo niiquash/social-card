a9257f393205ff1ec5eb61d27c7a392866b8e04e
niiquash <qua16006@byui.edu>
2025-01-05 19:14:23 -0700

    completed  location and email info sections

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -8,14 +8,14 @@ const Info = () => {
         <a href="#" className="info--location">
           <img
             src={Marker}
-            alt="marker icon"
+            alt="location icon"
             className="info--location__icon"
           />
           <span className="info--location__text">Kittenville</span>
         </a>
         <a href="#" className="info--email">
-          <img src={Email} alt="email icon" className="info--marker__icon" />
-          <span className="info--marker__text">Kittenville</span>
+          <img src={Email} alt="email icon" className="info--email__icon" />
+          <span className="info--email__text">Send Email</span>
         </a>
       </div>
       <div className="info--followers-following">
